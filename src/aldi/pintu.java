3dfaaded1e52b9950950270805c40ea7e127d178
aldii <akhmadaldi54@gmail.com>
2018-03-29 23:26:48 +0800

    membuatatribut dan behavior

--- src/aldi/pintu.java
+++ src/aldi/pintu.java
@@ -1,4 +1,22 @@
 package aldi;
 public class pintu {
-    
+    double lebar;
+      double panjang;
+
+    public pintu() {
+        panjang=35;
+        lebar=170;
+    }
+
+    public pintu(double lebar, double panjang) {
+        this.lebar = lebar;
+        this.panjang = panjang;
+    }
+      
+      
+      
+      void cetakInfo(){
+          System.out.println("Panjang       :"+lebar);
+          System.out.println("Panjang       :"+panjang); 
+    }
 }
