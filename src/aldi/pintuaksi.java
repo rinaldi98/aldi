1f2f2174ad425617199decde5c441b9b9d5654fa
aldii <akhmadaldi54@gmail.com>
2018-03-30 00:36:12 +0800

    membuat lebar dan panjang

--- src/aldi/pintuaksi.java
+++ src/aldi/pintuaksi.java
@@ -4,7 +4,8 @@ public class pintuaksi {
     public static void main(String[] args) {
         pintu r1 = new pintu();
         r1.panjang = 9;
-        r1.lebar = 4;
+        r1.lebar = 7;
+    
         
         r1.cetakInfo();
         System.out.println("Luas pintu: "+r1.hitungLuas());
